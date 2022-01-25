4ff3acdcdbd25ab5d6912b850a03c031751e4259
Duncan Mackenzie <duncanma@microsoft.com>
2022-01-24 17:11:03 -0800

    template updates, copy of .NET app to download UHF checked in

---
+++ uhf_downloader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShellModel shell = ShellServiceProvider.Load("en-us");
+
+            string partialsDirectory = @"C:\repos\MsftBlog\themes\microsoft\layouts\partials";
+            string cssFile = Path.Combine(partialsDirectory, "uhf_css.html");
+            string footerFile = Path.Combine(partialsDirectory, "uhf_footer.html");
+            string headerFile = Path.Combine(partialsDirectory, "uhf_header.html");
+            string javascriptFile = Path.Combine(partialsDirectory, "uhf_javascript.html");
+
+            File.WriteAllText(cssFile, shell.CssIncludes.ToString());
+            File.WriteAllText(footerFile, shell.FooterHtml.ToString());
+            File.WriteAllText(headerFile, shell.HeaderHtml.ToString());
+            File.WriteAllText(javascriptFile, shell.JavaScriptIncludes.ToString());
+
+        }
+    }
+}
---
+++ uhf_downloader/UHF.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Text;
+using System.Web;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    public class ShellModel
+    {
+        public HtmlString CssIncludes { get; set; }
+        public HtmlString JavaScriptIncludes { get; set; }
+        public HtmlString HeaderHtml { get; set; }
+        public HtmlString FooterHtml { get; set; }
+    }
+
+    public class ShellServiceProvider
+    {
+        static string serviceEndpoint = "https://uhf.microsoft.com";
+        static string partnerId = "MSMSRC";
+        static string headerId = "MSMSRCHeader";
+        static string footerId = "MSMSRCFooter";
+        static string userAgent = "Microsoft";
+
+        public static ShellModel Load(string locale)
+        {
+            string xml = string.Empty;
+            using (var client = new WebClient())
+            {
+                var serviceUrl = serviceEndpoint +
+                    "/" + locale + "/shell/xml/" + partnerId +
+                    "?headerId=" + headerId +
+                    "&footerId=" + footerId;
+                client.Headers.Add("user-agent", userAgent);
+                client.Encoding = Encoding.UTF8;
+                xml = client.DownloadString(serviceUrl);
+            }
+            return ConvertXmlToModel(xml);
+        }
+
+        private static ShellModel ConvertXmlToModel(string xml)
+        {
+            var document = XDocument.Parse(xml);
+            var root = document.Element("shell");
+            return new ShellModel
+            {
+                CssIncludes = EnsureStringValue(root, "cssIncludes"),
+                JavaScriptIncludes = EnsureStringValue(root, "javascriptIncludes"),
+                FooterHtml = EnsureStringValue(root, "footerHtml"),
+                HeaderHtml = EnsureStringValue(root, "headerHtml"),
+            };
+        }
+
+        private static System.Web.HtmlString EnsureStringValue(XElement root, string elementName)
+        {
+            var element = root.Element(elementName);
+            return new System.Web.HtmlString(element != null ? (string)element : string.Empty);
+        }
+    }
+}
